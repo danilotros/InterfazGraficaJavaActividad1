ddfe92dc40210e2ddfe721e34c0400b7871c7bc0
danilotros <danielgarcia10211@gmail.com>
2020-05-11 11:52:40 -0500

    Desarrollo de la clase

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        VistaPrincipalTemplate vista=new VistaPrincipalTemplate();
+    }
+}
---
+++ src/vistaPrincipal/VistaPrincipalTemplate.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+/**
+ * 
+ * Author Daniel Garcia Perea
+*/
+public class VistaPrincipalTemplate extends JFrame {
+    private JButton btnLog;
+    private JTextField userLog;
+    private JTextField passLog;
+    private JLabel luser;
+    private JLabel lpass;
+    
+public VistaPrincipalTemplate(){
+    setTitle("Discord Login");
+    this.mensaje("Daniel");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLayout(null);
+    setSize(1400,1200);
+    setLocationRelativeTo(this);
+    getContentPane().setBackground(Color.red);
+    setVisible(true);
+    
+
+}
+public void mensaje(String nombre){
+    System.out.println("Hola Bienvenido a Discord "+nombre);
+}
+}
